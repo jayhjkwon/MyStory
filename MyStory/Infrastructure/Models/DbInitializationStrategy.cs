8798b4164569d0e79e3b771c01213d97cb86b67c
Ray H.J Kwon <nicekhj@gmail.com>
2011-09-14 20:57:03 +0900

    changed namespace for MyStoryContext

---
+++ MyStory/Infrastructure/Models/DbInitializationStrategy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace MyStory.Infrastructure.Models
+{
+    public class DbInitializationStrategy : DropCreateDatabaseIfModelChanges<MyStoryContext>
+    {
+        protected override void Seed(MyStoryContext context)
+        {
+            base.Seed(context);
+        }
+    }
+}
