81d371b0ed0905b445f2896d7b0dc15eb7c28c5c
Ray H.J Kwon <nicekhj@gmail.com>
2011-09-14 21:00:38 +0900

    added DatabaseCreationTest.cs

---
+++ MyStory.Tests/Models/DatabaseCreationTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace MyStory.Tests.Models
+{
+    public class DatabaseCreationTest
+    {
+        [Fact]
+        public void test()
+        {
+            Assert.Equal(1, 1);
+        }
+    }
+}
