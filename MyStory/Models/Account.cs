326913321d16d9a8fda6640a5ea399fc12bca4f6
Ray H.J Kwon <nicekhj@gmail.com>
2011-11-02 21:31:29 +0900

    changed property name from AccountEmail to BlogOwnerEmain in Blog.cs

--- MyStory/Models/Account.cs
+++ MyStory/Models/Account.cs
@@ -5,18 +5,15 @@ using System.Web;
 
 namespace MyStory.Models
 {
-    /// <summary>
-    /// domain class for user account
-    /// </summary>
     public class Account
-    {
-        
+    {        
         public string Email { get; set; }
         public string FullName { get; set; }
         public string Password { get; set; }
         public bool IsOpenId { get; set; }
+        public string GravatarUrl { get; set; }
+    
         public int? BlogId { get; set; }
         public virtual Blog Blog { get; set; }
-        public string GravatarUrl { get; set; }
     }
 }
