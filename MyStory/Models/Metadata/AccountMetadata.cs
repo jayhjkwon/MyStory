c8623f8d9370bd2a485464d90838bd932f79caf5
Ray H.J Kwon <nicekhj@gmail.com>
2011-11-06 20:37:45 +0900

    add Metadata folder

---
+++ MyStory/Models/Metadata/AccountMetadata.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyStory.Models.Metadata
+{
+    public class AccountMetadata
+    {
+        [Required]
+        [StringLength(255)]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required]
+        [MinLength(6)]
+        [MaxLength(12)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
