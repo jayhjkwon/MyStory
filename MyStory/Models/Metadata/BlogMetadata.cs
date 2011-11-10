121c8c9f614801a4f930b256c19f25b69e6195be
Ray H.J Kwon <nicekhj@gmail.com>
2011-11-10 17:40:29 +0900

    added BlogMetadata.cs

---
+++ MyStory/Models/Metadata/BlogMetadata.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyStory.Models.Metadata
+{
+    public class BlogMetadata
+    {
+        [Required]
+        [MinLength(1)]
+        [MaxLength(125)]
+        public string Title { get; set; }
+    }
+}
