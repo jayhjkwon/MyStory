4ef40359259e7ded1dcece5998e949bec63cb108
Ray H.J Kwon <nicekhj@gmail.com>
2012-03-13 15:22:02 +0900

    changed a:hover style from underline to none

--- MyStory/ViewModels/PostDetailViewModel.cs
+++ MyStory/ViewModels/PostDetailViewModel.cs
@@ -10,5 +10,6 @@ namespace MyStory.ViewModels
         public int Id { get; set; }
         public string Title { get; set; }
         public string Content { get; set; }
+        public DateTime DateCreated { get; set; }
     }
 }
