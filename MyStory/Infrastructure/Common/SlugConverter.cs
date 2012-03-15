6bf3cca34a3f4f1c78a294b073206665b8f4a886
Ray H.J Kwon <nicekhj@gmail.com>
2012-03-15 17:05:16 +0900

    added SlugConverter class

---
+++ MyStory/Infrastructure/Common/SlugConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Text.RegularExpressions;
+
+namespace MyStory.Infrastructure.Common
+{
+    public class SlugConverter
+    {
+        public string CreateSlug(string Title)
+        {
+            string Slug = Title.ToLower();
+ 
+            // Replace - with empty space
+            Slug = Slug.Replace("-", " ");
+
+            // Replace unwanted characters with space
+            Slug = Regex.Replace(Slug, @"[^a-z0-9\s-]", " ");
+            
+            // Replace multple white spaces with single space
+            Slug = Regex.Replace(Slug, @"\s+", " ").Trim();
+            
+            // Replace white space with -
+            Slug = Slug.Replace(" ", "-");
+ 
+            return Slug;
+        }
+    }
+}
