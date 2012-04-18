14c76b3466dfc364e2cbb01fcd6f83b5bd6e1141
Ray H.J Kwon <nicekhj@gmail.com>
2012-04-18 17:18:44 +0900

    added test for comment controller

--- MyStory.Tests/FunctionalTests/Controllers/CommentControllerTest.cs
+++ MyStory.Tests/FunctionalTests/Controllers/CommentControllerTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Text;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,27 +15,27 @@ namespace MyStory.Tests.FunctionalTests.Controllers
     [TestClass]
     public class CommentControllerTest
     {
-        private CommentController controller;
-        private MyStoryContext dbContext;
+        private CommentController _controller;
+        private MyStoryContext _dbContext;
 
         // Use TestInitialize to run code before running each test 
         [TestInitialize()]
         public void TestInitialize()
         {
             // Arrange
-            dbContext = new MyStoryContext();
-            dbContext.Database.Delete();
-            dbContext.Database.Create();
+            _dbContext = new MyStoryContext();
+            _dbContext.Database.Delete();
+            _dbContext.Database.Create();
         }
 
         // Use TestCleanup to run code after each test has run
         [TestCleanup()]
         public void TestCleanup()
         {
-            if (controller != null)
+            if (_controller != null)
             {
-                dbContext.Database.Delete();
-                controller.Dispose();
+                _dbContext.Database.Delete();
+                _controller.Dispose();
             }
         }
 
@@ -42,15 +43,16 @@ namespace MyStory.Tests.FunctionalTests.Controllers
         public void sidebar_method_should_return_comment()
         {
             FunctionalTestHelper.CreateAutomapperMap();
-            FunctionalTestHelper.CreateAccountAndBlog(dbContext);
-            FunctionalTestHelper.CreateOnePost(dbContext);
-            FunctionalTestHelper.CreateOneCommenter(dbContext);
-            FunctionalTestHelper.CreateOneComment(dbContext);
+            FunctionalTestHelper.CreateAccountAndBlog(_dbContext);
+            FunctionalTestHelper.CreateOnePost(_dbContext);
+            FunctionalTestHelper.CreateOneCommenter(_dbContext);
+            FunctionalTestHelper.CreateOneComment(_dbContext);
 
-            controller = new CommentController();
+            _controller = new CommentController();
 
-            var result = controller.Sidebar() as ViewResult;
-            
+            var result = _controller.Sidebar() as ViewResult;
+
+            result.ShouldNotBeNull();
             result.ViewName.ShouldEqual("Sidebar");
             var model = result.Model as List<CommentSidebarViewModel>;
             model.ShouldNotBeNull();
@@ -61,15 +63,15 @@ namespace MyStory.Tests.FunctionalTests.Controllers
         public void comment_should_less_then_50()
         {
             FunctionalTestHelper.CreateAutomapperMap();
-            FunctionalTestHelper.CreateAccountAndBlog(dbContext);
-            FunctionalTestHelper.CreateOnePost(dbContext);
-            FunctionalTestHelper.CreateOneCommenter(dbContext);
+            FunctionalTestHelper.CreateAccountAndBlog(_dbContext);
+            FunctionalTestHelper.CreateOnePost(_dbContext);
+            FunctionalTestHelper.CreateOneCommenter(_dbContext);
             FunctionalTestHelper.CommentContent = "GreaterThan50GreaterThan50GreaterThan50GreaterThan50GreaterThan50";
-            FunctionalTestHelper.CreateOneComment(dbContext);
+            FunctionalTestHelper.CreateOneComment(_dbContext);
 
-            controller = new CommentController();
+            _controller = new CommentController();
 
-            var result = controller.Sidebar() as ViewResult;
+            var result = _controller.Sidebar() as ViewResult;
 
             result.ViewName.ShouldEqual("Sidebar");
             var model = result.Model as List<CommentSidebarViewModel>;
@@ -77,5 +79,83 @@ namespace MyStory.Tests.FunctionalTests.Controllers
             model.Count.ShouldEqual(1);
             model.First().Content.Length.ShouldEqual(50);
         }
+
+        [TestMethod]
+        public void postcommentslist_method_should_validate_postid_isnull()
+        {
+            _controller = new CommentController();
+            var result = _controller.PostCommentsList(null) as ViewResult;
+            result.ViewName.ShouldEqual("PostCommentsList");
+            result.Model.ShouldBeNull();
+        }
+
+        [TestMethod]
+        public void postcommentslist_method_should_return_model()
+        {
+            FunctionalTestHelper.CreateAutomapperMap();
+            FunctionalTestHelper.CreateAccountAndBlog(_dbContext);
+            FunctionalTestHelper.CreateOnePost(_dbContext);
+            FunctionalTestHelper.CreateOneCommenter(_dbContext);
+            FunctionalTestHelper.CreateOneComment(_dbContext);
+
+            _controller = new CommentController();
+            var result = _controller.PostCommentsList(1) as ViewResult;
+            
+            result.ViewName.ShouldEqual("PostCommentsList");
+            var model = result.Model as PostCommentsViewModel;
+            model.ShouldNotBeNull();
+            model.PostId.ShouldEqual(1);
+            model.Comments.ShouldNotBeNull();
+            model.Comments.Count.ShouldEqual(1);
+        }
+
+        [TestMethod]
+        public void write_should_validate_model()
+        {
+            _controller = new CommentController();
+            _controller.ModelState.AddModelError("modelerror","modelerror");
+
+            var result = _controller.Write(new CommentInput(), 0) as RedirectToRouteResult;
+
+            result.RouteValues["controller"].ShouldEqual("Post");
+            result.RouteValues["action"].ShouldEqual("Detail");
+            result.RouteValues["id"].ShouldEqual(0);
+            result.RouteValues["errorFromCommentInput"].ShouldEqual(true);
+
+            _controller.TempData["commentInputData"].ShouldNotBeNull();
+            _controller.TempData["commentInputDataErrors"].ShouldNotBeNull();
+            
+        }
+
+        [TestMethod]
+        public void write_should_save_comment()
+        {
+            FunctionalTestHelper.CreateAutomapperMap();
+            FunctionalTestHelper.CreateAccountAndBlog(_dbContext);
+            FunctionalTestHelper.CreateOnePost(_dbContext);
+            FunctionalTestHelper.CreateOneCommenter(_dbContext);
+
+            _controller = new CommentController();
+
+            var commentInput = new CommentInput
+                                   {
+                                       Content = "content",
+                                       Email = FunctionalTestHelper.CommenterEmail,
+                                       Name = FunctionalTestHelper.CommenterName,
+                                   };
+            var result = _controller.Write(commentInput, 1) as RedirectToRouteResult;
+
+            result.RouteValues["controller"].ShouldEqual("Post");
+            result.RouteValues["action"].ShouldEqual("Detail");
+            result.RouteValues["id"].ShouldEqual(1);
+
+            _dbContext.Comments.Count().ShouldEqual(1);
+            var comment = _dbContext.Comments.FirstOrDefault();
+            comment.Content.ShouldEqual("content");
+            comment.Commenter.Email.ShouldEqual(FunctionalTestHelper.CommenterEmail);
+            comment.Commenter.Name.ShouldEqual(FunctionalTestHelper.CommenterName);
+
+        }
     }
 }
+
