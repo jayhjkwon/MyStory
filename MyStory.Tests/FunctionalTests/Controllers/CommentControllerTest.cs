3f856bd7f2a782c337648b300f5dbe41c64ae33d
Ray H.J Kwon <nicekhj@gmail.com>
2012-04-18 14:52:00 +0900

    added test for /comment/sidebar

---
+++ MyStory.Tests/FunctionalTests/Controllers/CommentControllerTest.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyStory.Controllers;
+using MyStory.Models;
+using System.Data.Entity;
+using System.Web.Mvc;
+using MyStory.ViewModels;
+
+namespace MyStory.Tests.FunctionalTests.Controllers
+{
+    [TestClass]
+    public class CommentControllerTest
+    {
+        private CommentController controller;
+        private MyStoryContext dbContext;
+
+        // Use TestInitialize to run code before running each test 
+        [TestInitialize()]
+        public void TestInitialize()
+        {
+            // Arrange
+            dbContext = new MyStoryContext();
+            dbContext.Database.Delete();
+            dbContext.Database.Create();
+        }
+
+        // Use TestCleanup to run code after each test has run
+        [TestCleanup()]
+        public void TestCleanup()
+        {
+            if (controller != null)
+            {
+                dbContext.Database.Delete();
+                controller.Dispose();
+            }
+        }
+
+        [TestMethod]
+        public void sidebar_method_should_return_comment()
+        {
+            FunctionalTestHelper.CreateAutomapperMap();
+            FunctionalTestHelper.CreateAccountAndBlog(dbContext);
+            FunctionalTestHelper.CreateOnePost(dbContext);
+            FunctionalTestHelper.CreateOneCommenter(dbContext);
+            FunctionalTestHelper.CreateOneComment(dbContext);
+
+            controller = new CommentController();
+
+            var result = controller.Sidebar() as ViewResult;
+            
+            result.ViewName.ShouldEqual("Sidebar");
+            var model = result.Model as List<CommentSidebarViewModel>;
+            model.ShouldNotBeNull();
+            model.Count.ShouldEqual(1);
+        }
+
+        [TestMethod]
+        public void comment_should_less_then_50()
+        {
+            FunctionalTestHelper.CreateAutomapperMap();
+            FunctionalTestHelper.CreateAccountAndBlog(dbContext);
+            FunctionalTestHelper.CreateOnePost(dbContext);
+            FunctionalTestHelper.CreateOneCommenter(dbContext);
+            FunctionalTestHelper.CommentContent = "GreaterThan50GreaterThan50GreaterThan50GreaterThan50GreaterThan50";
+            FunctionalTestHelper.CreateOneComment(dbContext);
+
+            controller = new CommentController();
+
+            var result = controller.Sidebar() as ViewResult;
+
+            result.ViewName.ShouldEqual("Sidebar");
+            var model = result.Model as List<CommentSidebarViewModel>;
+            model.ShouldNotBeNull();
+            model.Count.ShouldEqual(1);
+            model.First().Content.Length.ShouldEqual(50);
+        }
+    }
+}
---
+++ MyStory.Tests/MvcMockHelpers.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using System.Collections.Specialized;
+using Moq;
+using System.Web.Routing;
+using System.Security.Principal;
+
+namespace MyStory.Tests
+{
+    public static class MvcMockHelpers
+    {
+        public static bool _IsAuthenticated { get; set; }
+        public static bool _IsAjaxRequest { get; set; }
+
+        public static HttpContextBase FakeHttpContext()
+		{
+            var context = new Mock<HttpContextBase>();
+            var files = new Mock<HttpFileCollectionBase>();
+            var request = new Mock<HttpRequestBase>();
+            var response = new Mock<HttpResponseBase>();
+            var session = new Mock<HttpSessionStateBase>();
+            var server = new Mock<HttpServerUtilityBase>();
+            var user = new Mock<IPrincipal>();
+            var identity = new Mock<IIdentity>();
+
+            request.Setup(req => req.ApplicationPath).Returns("~/");
+            request.Setup(req => req.AppRelativeCurrentExecutionFilePath).Returns("~/");
+            request.Setup(req => req.PathInfo).Returns(string.Empty);
+            request.Setup(req => req.Form).Returns(new NameValueCollection());
+            request.Setup(req => req.QueryString).Returns(new NameValueCollection());
+            request.Setup(req => req.Files).Returns(files.Object);
+            request.Setup(req => req.IsAuthenticated).Returns(_IsAuthenticated);
+            if(_IsAjaxRequest)
+                request.Setup(req => req["X-Requested-With"]).Returns("XMLHttpRequest");
+
+            response.Setup(res => res.ApplyAppPathModifier(It.IsAny<string>())).
+                Returns((string virtualPath) => virtualPath);
+            
+            user.Setup(usr => usr.Identity).Returns(identity.Object);
+            user.Setup(usr => usr.Identity.Name).Returns("a@a.com");
+            
+            identity.Setup(ident => ident.IsAuthenticated).Returns(true);
+
+            context.Setup(ctx => ctx.Request).Returns(request.Object);
+            context.Setup(ctx => ctx.Response).Returns(response.Object);
+            context.Setup(ctx => ctx.Session).Returns(session.Object);
+            context.Setup(ctx => ctx.Server).Returns(server.Object);
+            context.Setup(ctx => ctx.User).Returns(user.Object);
+
+            return context.Object;
+		}
+
+		public static HttpContextBase FakeHttpContext(string url)
+		{
+			HttpContextBase context = FakeHttpContext();
+			context.Request.SetupRequestUrl(url);
+			return context;
+		} 
+
+		public static void SetFakeControllerContext(this Controller controller,
+                                                    bool isAuthenticated = true,
+                                                    bool isAjaxRequest = false)
+		{
+            _IsAuthenticated = isAuthenticated;
+            _IsAjaxRequest = isAjaxRequest;
+
+			var httpContext = FakeHttpContext();
+			
+            ControllerContext context = new ControllerContext(
+                new RequestContext(httpContext, new RouteData()),
+                controller);
+
+			controller.ControllerContext = context;
+		}
+
+		static string GetUrlFileName(string url)
+		{
+			if (url.Contains("?"))
+				return url.Substring(0, url.IndexOf("?"));
+			else
+				return url;
+		}
+
+		static NameValueCollection GetQueryStringParameters(string url)
+		{
+			if (url.Contains("?"))
+			{
+				NameValueCollection parameters = new NameValueCollection();
+
+				string[] parts = url.Split("?".ToCharArray());
+				string[] keys = parts[1].Split("&".ToCharArray());
+
+				foreach (string key in keys)
+				{
+					string[] part = key.Split("=".ToCharArray());
+					parameters.Add(part[0], part[1]);
+				}
+
+				return parameters;
+			}
+			else
+			{
+				return null;
+			}
+		}
+
+		public static void SetHttpMethodResult(this HttpRequestBase request, string httpMethod)
+		{
+			Mock.Get(request)
+				.Setup(req => req.HttpMethod)
+				.Returns(httpMethod);
+		}
+
+		public static void SetupRequestUrl(this HttpRequestBase request, string url)
+		{
+			if (url == null)
+				throw new ArgumentNullException("url");
+
+			if (!url.StartsWith("~/"))
+				throw new ArgumentException("Sorry, we expect a virtual url starting with \"~/\".");
+
+			var mock = Mock.Get(request);
+
+            mock.Setup(req => req.QueryString)
+				.Returns(GetQueryStringParameters(url));
+            mock.Setup(req => req.AppRelativeCurrentExecutionFilePath)
+				.Returns(GetUrlFileName(url));
+            mock.Setup(req => req.PathInfo)
+				.Returns(string.Empty);
+		}
+	}
+}
